b516303d87f4bdf7f633814807c7b1b31dea611e
yusuf afandi <afandi.yusuf.04@gmail.com>
2022-07-08 15:07:37 +0700

    add region to the android sdk

--- android/src/main/java/com/example/simple_aws_translate/SimpleAwsTranslatePlugin.java
+++ android/src/main/java/com/example/simple_aws_translate/SimpleAwsTranslatePlugin.java
@@ -10,64 +10,72 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.handlers.AsyncHandler;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.translate.AmazonTranslateAsyncClient;
 import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.amazonaws.services.translate.model.TranslateTextResult;
+import com.amazonaws.services.translate.model.TranslationSettings;
 
-/** SimpleAwsTranslatePlugin */
+/**
+ * SimpleAwsTranslatePlugin
+ */
 public class SimpleAwsTranslatePlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "simple_aws_translate");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("translateText")) {
-      String accessKey = call.argument("accessKey");
-      String secretKey = call.argument("secretKey");
-      AWSCredentials awsCredentials = new AWSCredentials() {
-        @Override
-        public String getAWSAccessKeyId() {
-          return accessKey;
-        }
-
-        @Override
-        public String getAWSSecretKey() {
-          return secretKey;
-        }
-      };
-      AmazonTranslateAsyncClient translateAsyncClient = new AmazonTranslateAsyncClient(awsCredentials);
-      TranslateTextRequest translateTextRequest = new TranslateTextRequest()
-              .withText(call.argument("text"))
-              .withSourceLanguageCode(call.argument("from"))
-              .withTargetLanguageCode(call.argument("to"));
-      translateAsyncClient.translateTextAsync(translateTextRequest,new AsyncHandler<TranslateTextRequest,TranslateTextResult>(){
-
-        @Override
-        public void onError(Exception exception) {
-          result.error(exception.getMessage(),"",null);
-        }
-
-        @Override
-        public void onSuccess(TranslateTextRequest request, TranslateTextResult translateTextResult) {
-          result.success(translateTextResult.getTranslatedText());
-        }
-      });
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "simple_aws_translate");
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("translateText")) {
+            String accessKey = call.argument("accessKey");
+            String secretKey = call.argument("secretKey");
+            AWSCredentials awsCredentials = new AWSCredentials() {
+                @Override
+                public String getAWSAccessKeyId() {
+                    return accessKey;
+                }
+
+                @Override
+                public String getAWSSecretKey() {
+                    return secretKey;
+                }
+            };
+
+
+            AmazonTranslateAsyncClient translateAsyncClient = new AmazonTranslateAsyncClient(awsCredentials);
+            TranslateTextRequest translateTextRequest = new TranslateTextRequest()
+                    .withText(call.argument("text"))
+                    .withSourceLanguageCode(call.argument("from"))
+                    .withTargetLanguageCode(call.argument("to"));
+            translateAsyncClient.setRegion(Region.getRegion(Regions.AP_SOUTHEAST_1));
+            translateAsyncClient.translateTextAsync(translateTextRequest, new AsyncHandler<TranslateTextRequest, TranslateTextResult>() {
+
+                @Override
+                public void onError(Exception exception) {
+                    result.error(exception.getMessage(), "", null);
+                }
+
+                @Override
+                public void onSuccess(TranslateTextRequest request, TranslateTextResult translateTextResult) {
+                    result.success(translateTextResult.getTranslatedText());
+                }
+            });
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 }
